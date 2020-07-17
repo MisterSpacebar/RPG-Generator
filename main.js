4bbe3fa3ae3b0b5a198e26ee000a39a3c2830156
Yibin <yibinwei@live.com>
2020-07-17 18:53:27 -0400

    more edit

--- main.js
+++ main.js
@@ -94,7 +94,7 @@ $("#remake").on("click", function(event){
     $("#armor").text("Armor: "+armor[randomGen(armor.length)]);
     $("#potion").text("Extra: "+potion[randomGen(potion.length)]);
 
-    $("#currency").text("Starting Currency: "+(50+randomGen(50)+randomGen(50))+" Coins");
+    $("#currency").text("Starting Currency: "+(47+randomGen(55)+randomGen(55)+randomGen(55))+" Coins");
 
     // console.log(desires[desireNumber]);
     // console.log(flaws[flawNumber]);
